e91bbafa528da6659f1715db67a9049934d057be
gamerAbby919 <ralston.abb8916@stu.stech.edu>
2024-11-05 08:33:37 -0700

    Exercise 12-15 Implementation
    Does the same thing as Exercise 12-15 Planning, but sorts the numbers from smallest to largest.

---
+++ Chapter 12/Exercise1215Implementation.java
@@ -0,0 +1,31 @@
+import java.nio.*;
+import java.io.*;
+import java.util.*;
+		
+class Exercise1215Implementation {
+	public static void main(String[] args) throws Exception{
+		java.io.File file = new java.io.File("Exercise12_15.txt");
+		Scanner input = new Scanner(new File("Exercise12_15.txt"));
+		int[] myList = new int[100];
+				
+			try (
+				java.io.PrintWriter output = new java.io.PrintWriter(file);
+				) {
+					output.print(myList);
+					output.close();
+				for (int i = 0; i < myList.length; i++) {
+					myList[i] = (int)(Math.random() * 100);
+					output.print(myList[i] + " ");
+					}
+				}
+				catch (FileNotFoundException noFile) {
+					System.out.println("Error");
+				}
+				
+				for (int i = 0; i < myList.length; i++) {
+						myList[i] = (int)(Math.random() * 100);
+						Arrays.sort(myList);
+						System.out.println(myList[i] + " ");
+				}
+			}
+		}
